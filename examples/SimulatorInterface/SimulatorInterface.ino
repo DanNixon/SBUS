ad216cca46b6092b1aaabba5016d315a50137e5d
Dan Nixon <dan@dan-nixon.com>
2016-02-15 22:28:16 +0000

    Update simulator

--- examples/SimulatorInterface/SimulatorInterface.ino
+++ examples/SimulatorInterface/SimulatorInterface.ino
@@ -5,8 +5,8 @@
 
 SBUS sbus(Serial3);
 
-#define SBUS_LOW -75
-#define SBUS_HIGH 75
+#define SBUS_HIGH 83
+#define SBUS_LOW -SBUS_HIGH
 
 enum Channel
 {
@@ -37,12 +37,18 @@ void loop()
 {
   delay(5);
 
-  Joystick.X(map(sbus.getNormalizedChannel(CH_PITCH), SBUS_LOW, SBUS_HIGH, 0, 1023));
-  Joystick.Y(map(sbus.getNormalizedChannel(CH_ROLL), SBUS_LOW, SBUS_HIGH, 0, 1023));
+  Joystick.Y(map(sbus.getNormalizedChannel(CH_PITCH), SBUS_LOW, SBUS_HIGH, 0, 1023));
+  Joystick.X(map(sbus.getNormalizedChannel(CH_ROLL), SBUS_LOW, SBUS_HIGH, 0, 1023));
   Joystick.Z(map(sbus.getNormalizedChannel(CH_THROT), SBUS_LOW, SBUS_HIGH, 0, 1023));
   Joystick.Zrotate(map(sbus.getNormalizedChannel(CH_YAW), SBUS_LOW, SBUS_HIGH, 0, 1023));
+
   Joystick.sliderLeft(map(sbus.getNormalizedChannel(5), SBUS_LOW, SBUS_HIGH, 0, 1023));
   Joystick.sliderRight(map(sbus.getNormalizedChannel(6), SBUS_LOW, SBUS_HIGH, 0, 1023));
+
+  Joystick.button(1, sbus.getNormalizedChannel(7) >= 60);
+  Joystick.button(2, sbus.getNormalizedChannel(8) >= 60);
+  Joystick.button(3, sbus.getNormalizedChannel(9) >= 60);
+  Joystick.button(4, sbus.getNormalizedChannel(10) >= 60);
 }
 
 void up()
